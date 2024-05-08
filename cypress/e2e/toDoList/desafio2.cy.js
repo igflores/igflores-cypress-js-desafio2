3f26188c2f7a37c4d7873e9ad3e64e72ed273b52
igflores <139146133+igflores@users.noreply.github.com>
2024-05-08 13:29:51 -0500

    test(desafio2-pushing-it): Updated all test, all working. Pending: Xpath

--- cypress/e2e/toDoList/desafio2.cy.js
+++ cypress/e2e/toDoList/desafio2.cy.js
@@ -12,25 +12,26 @@ describe('Actividad complementaria 5', () => {
         cy.get('#submitForm').click()
         cy.get(`[id*='user_pushingit']`).should('exist')
         cy.get('[data-cy="todolistlink"]').click(); 
+
+        cy.get('[data-cy="removeAll"]').should('be.visible').click();
+        cy.get('.css-tdm0fr').should('not.contain','button','Delete');
         
     })
 
     it('TC01: Should add 5 new tasks', () => {
         let count = 0;
-
         for(let i = 0; i < 5; i++){
             let numero = Math.floor(Math.random() * 100000);
-            cy.get('[data-cy="task"]').type(`Mi tarea:  ${numero}`);
+            cy.get('[data-cy="task"]').type(`Mi tarea #${numero}`);
             cy.get('[data-cy="sendTask"]').click();
             count++;
 
-            cy.get('[class="css-tdm0fr"] p', {timeout:2000}).last().should('have.text',`Mi tarea:  ${numero}`);
+            cy.get('[class="css-tdm0fr"] p', {timeout:2000}).last().should('have.text',`Mi tarea #${numero}`);
             cy.get('[class="css-tdm0fr"] p').should('have.length',count);
         };
-        cy.get('[class="css-tdm0fr"] p').should('have.length', 5);
-        cy.log(`La cantidad de tareas adicionadas es: ${count}`);
+        cy.get('[class="css-tdm0fr"] p').should('have.length', consT.Act5.numberOfTasks);
     })
-    
+
     it('TC02: Should exist the buttons “All”, “Completed”, “Active” y “Remove all”', () => {
         cy.get('[data-cy="all"]').should('be.visible').and('have.text', consT.Act5.allButtonText);
         cy.get('[data-cy="completed"]').should('be.visible').and('have.text',consT.Act5.completedButtonText);
@@ -38,34 +39,43 @@ describe('Actividad complementaria 5', () => {
         cy.get('[data-cy="removeAll"]').should('be.visible').and('have.text', consT.Act5.removeAllButtonText);
     })
 
-    // it.only('TC03: Should add 2 tasks, complete them, and delete the second one' () => {
-    //     for(let i = 0; i < 2; i++){
-    //         cy.get('[data-cy="task"]').type("Mi tarea: " + numero);       
-    //         cy.get('[data-cy="sendTask"]').click();
-    //     };
+    it('TC03: Should add 2 tasks, complete them, and delete the second one', () => {
+        let count = 0;
+        for(let i = 0; i < 2; i++){
+            let numero = Math.floor(Math.random() * 100000);
+            cy.get('[data-cy="task"]').type(`Mi tarea #${numero}`);
+            cy.get('[data-cy="sendTask"]').click();
+            count++;
 
-    // })
+            cy.get('[class="css-tdm0fr"] p', {timeout:2000}).last().should('have.text',`Mi tarea #${numero}`);
+            cy.get('[class="css-tdm0fr"] p').should('have.length',count);
+        };
+        cy.get('[class="css-tdm0fr"] p').should('have.length', consT.Act5.twoTasks);
 
+        cy.get('[class="css-tdm0fr"] button[class$="css-1evfvqt"]').last().click();
+        cy.get('[class="css-tdm0fr"] p').should('have.length', consT.Act5.twoTasks - 1);
+    })
+
+
+    it('TC04: Should add 2 tasks, complete them, and delete the first one', () => {
+        let count = 0;
+        for(let i = 0; i < 2; i++){
+            let numero = Math.floor(Math.random() * 100000);
+            cy.get('[data-cy="task"]').type(`Mi tarea #${numero}`);
+            cy.get('[data-cy="sendTask"]').click();
+            count++;
+
+            cy.get('[class="css-tdm0fr"] p', {timeout:2000}).last().should('have.text',`Mi tarea #${numero}`);
+            cy.get('[class="css-tdm0fr"] p').should('have.length',count);
+        };
+        cy.get('[class="css-tdm0fr"] p').should('have.length', consT.Act5.twoTasks);
+
+        cy.get('[class="css-tdm0fr"] button[class$="css-1evfvqt"]').first().click();
+        cy.get('[class="css-tdm0fr"] p').should('have.length', consT.Act5.twoTasks - 1);
+    })
 
     afterEach('Remove all tasks', () => {
         cy.get('[data-cy="removeAll"]').should('be.visible').click();
         cy.get('.css-tdm0fr').should('not.contain','button','Delete');
     })
-    
-
-
-    //Todo: wait
-    // it("Deberia verificar que el boton 'cargando' su texto vuelva a ser 'button", () =>{
-    //     cy.get('#wait').should('have.text','Cargando').wait(consT.Act5.timeout10s);
-    //     cy.get('#wait').should('have.text','Button');
-    // })    
-
-    // it("Deberia verificar que exista un elemento cuyo texto sea 'Wait 5 more seconds'", () =>{
-    //     cy.get('[data-cy="colorChangeMessage"]', {timeout: consT.Act5.timeout10s}).should('have.text', consT.Act5.messageWait_5seconds);
-	// })
-
-    // it("Verificar que el primer mensaje que aparece a los 10 luego de 5 segundos mas sea diferente. (que cambie el mensaje)", () => {
-    //     cy.get('[data-cy="message"]', {timeout: consT.Act5.timeout10s}).should('have.text',consT.Act5.messageWaited_5seconds).wait(consT.Act5.timeout5s);
-    //     cy.get('[data-cy="message"]', {timeout: consT.Act5.timeout5s}).should('have.text', consT.Act5.messageManOfPatience);
-	// })
 })
--- cypress/support/utils/constants.js
+++ cypress/support/utils/constants.js
@@ -2,16 +2,12 @@ module.exports = {
     Act5: {
         "username": "pushingit",
         "password": "123456!",
-
+        "numberOfTasks": 5,
         "allButtonText":"All",
         "completedButtonText":"Completed",
         "activeButtonText":"Active",
         "removeAllButtonText":"Remove all",
 
-        "timeout10s": 10000,
-        "timeout5s": 5000,
-        "messageWait_5seconds": "Wait 5 more seconds",
-        "messageWaited_5seconds":"You have waited for ten seconds, CONGRATULATIONS",
-        "messageManOfPatience":"You are a man of patience and have waited fifteen seconds",
+        "twoTasks": 2,
     }
 }
